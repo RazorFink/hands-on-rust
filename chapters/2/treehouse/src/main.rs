184431b964b2f9e9589af802923c613fd8d5fae6
Matt Howe <mhowe@bates.edu>
2020-12-22 17:37:46 -0500

    Move name capture to its own function

--- chapters/2/treehouse/src/main.rs
+++ chapters/2/treehouse/src/main.rs
@@ -1,10 +1,14 @@
 #![warn(clippy::all, clippy::pedantic)]
 use std::io::stdin;
-fn main() {
-    println!("Hello, what's your name?");
+fn what_is_your_name() -> String{
     let mut your_name = String::new();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read a line!");
-    println!("Well, howdy {}!", your_name);
+    your_name
+}
+
+fn main() {
+    println!("Hello, what's your name?");
+    println!("Well, howdy {}!", what_is_your_name());
 }
