114c168a638dc179b3cd92a33436f03b9a844649
Matt Howe <mhowe@bates.edu>
2020-12-22 20:14:46 -0500

    Loop until exit condition (`name.is_empty()`)
    Make `visitor_list` mutable
    `push()` to `visitor_list` if name isn't found and not  blank
    Pretty-print Debug `{:#?}` list of Visitor structs

--- chapters/2/treehouse/src/main.rs
+++ chapters/2/treehouse/src/main.rs
@@ -28,17 +28,29 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
-    let visitor_list = vec![
+    let mut visitor_list = vec![
         Visitor::new("bert", "Hi Bert, would you like a sandwich?"),
         Visitor::new("fred", "Hi Fred, how's it hangin?"),
         Visitor::new("steve", "Is that Steve?  Steeeeve..."),
         Visitor::new("carl", "What the fuck're you doing Carl?"),
     ];
-    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
-    match known_visitor {
-        Some(visitor) => visitor.extend_greeting(),
-        None => println!("You are not known to us..."),
+    loop {
+        println!("Hello, what's your name?");
+        println!("Enter a blank line to quit.");
+        let name = what_is_your_name();
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.extend_greeting(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("You are not known to us...adding to the list!");
+                    visitor_list.push(Visitor::new(&name, "New friend"));
+                }
+            }
+        }
     }
+    println!("The final list:");
+    println!("{:#?}", visitor_list);
 }
