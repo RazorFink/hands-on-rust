01c249b8f51d78ecb21d3075cfaf36a4c4f6280e
Matt Howe <mhowe@bates.edu>
2020-12-22 19:39:31 -0500

    Add a Struct for Visior
    Include an `impl` with Struct Functions
    Including a constructor called `new` that refers to the type as `Self`
    Populate the array with `new::Visitor` instances
    Convert the array to an Iterable with `.iter()`
    Use a closure to `.filter()` the visitor list by name
    Match on the __Option__ returned from the `iter().find()`
    Print different messages if `Some()` are found vs. `None`

--- chapters/2/treehouse/src/main.rs
+++ chapters/2/treehouse/src/main.rs
@@ -1,5 +1,23 @@
 #![warn(clippy::all, clippy::pedantic)]
 use std::io::stdin;
+struct Visitor {
+    name: String,
+    greeting: String
+}
+
+impl Visitor {
+
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string()
+        }
+    }
+    fn extend_greeting(&self) {
+        println!("{}.", self.greeting);
+    }
+}
+
 fn what_is_your_name() -> String{
     let mut your_name = String::new();
     stdin()
@@ -13,17 +31,19 @@ fn what_is_your_name() -> String{
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Well, howdy {}!", name);
-    let visitor_list = ["bert", "fred", "steve", "carl"];
-    let mut is_allowed = false;
-    for visitor in &visitor_list {
-        if &name == visitor {
-            is_allowed = true;
-        }
+    let visitor_list = [
+        Visitor::new("bert", "Hi Bert, would you like a sandwich?"), 
+        Visitor::new("fred", "Hi Fred, how's it hangin?"), 
+        Visitor::new("steve", "Is that Steve?  Steeeeve..."),
+        Visitor::new("carl", "What the fuck're you doing Carl?"),
+    ];
+    let known_visitor = visitor_list
+        .iter()
+        .find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.extend_greeting(),
+        None => println!("You are not known to us...")
     }
-    if is_allowed {
-        println!("You're on the list!");
-    } else { 
-        println!("You're not on the list...");
-    }
+
 }
