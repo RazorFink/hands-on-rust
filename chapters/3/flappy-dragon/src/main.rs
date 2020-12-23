4908d4892ac260ca51b9c64c9151a56cfe12e2a8
Matt Howe <mhowe@bates.edu>
2020-12-23 15:28:33 -0500

    Implement the GameState trait for the State struct
    GameState is defined in the Bracket Lib library

--- chapters/3/flappy-dragon/src/main.rs
+++ chapters/3/flappy-dragon/src/main.rs
@@ -2,5 +2,13 @@
 use bracket_lib::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    struct State {}
+
+    impl GameState for State {
+        fn tick(&mut self, ctx: &mut BTerm) {
+            ctx.cls();
+            ctx.print(1, 1, "Hello Bracket Terminal!");
+        }
+
+    }
 }
