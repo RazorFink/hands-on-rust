d42b9ec8abd525e97862c7b9e904977b569a5a85
Matt Howe <mhowe@bates.edu>
2020-12-23 16:07:39 -0500

    Enumerate game modes
    Add `mode` to `State` struct
    Add `State` implementation with constructor
    Use `BTermBuilder` to build a `context` with chained functions
    Return `Result` from `main_loop` to `main` function
        which returns a `BError`

--- chapters/3/flappy-dragon/src/main.rs
+++ chapters/3/flappy-dragon/src/main.rs
@@ -1,14 +1,37 @@
 #![warn(clippy::all, clippy::pedantic)]
 use bracket_lib::prelude::*;
 
-fn main() {
-    struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
-    impl GameState for State {
-        fn tick(&mut self, ctx: &mut BTerm) {
-            ctx.cls();
-            ctx.print(1, 1, "Hello Bracket Terminal!");
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State { 
+            mode: GameMode::Menu, 
         }
-
     }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
+
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    main_loop(context, State::new())
+
 }
