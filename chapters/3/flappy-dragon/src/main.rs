9c5513d789d9dc1d415590952ec0afebcd31e63b
Matt Howe <mhowe@bates.edu>
2020-12-24 10:56:13 -0500

    Add obstacle to State

--- chapters/3/flappy-dragon/src/main.rs
+++ chapters/3/flappy-dragon/src/main.rs
@@ -95,7 +95,9 @@ enum GameMode {
 struct State {
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
     mode: GameMode,
+    score: i32,
 }
 
 impl State {
@@ -103,7 +105,9 @@ impl State {
         State { 
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu, 
+            score: 0,
         }
     }
 
