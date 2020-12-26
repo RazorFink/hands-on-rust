c6f8d1913da496279e53e5a8cf554f8347059f34
Matt Howe <mhowe@bates.edu>
2020-12-26 17:09:01 -0500

    Add Map struct
    Add function for calculating the row-first Map index from coordinates
    Start impl of Map
    Use the vec! macro to fill the screen-sized `tiles` vector with `FileType::Floor`

--- chapters/5/dungeoncrawl/src/map.rs
+++ chapters/5/dungeoncrawl/src/map.rs
@@ -6,6 +6,22 @@ pub const NUM_TILES: usize = (SCREEN_HEIGHT * SCREEN_WIDTH) as usize;
 #[derive(Copy, Clone, PartialEq)]
 enum TileType {
     Wall,
-    Floor
+    Floor,
 }
 
+struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+pub fn map_idx(x: i32, y:i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
+impl Map {
+    fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+
+    }
+}
