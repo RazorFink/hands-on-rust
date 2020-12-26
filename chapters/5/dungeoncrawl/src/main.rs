5917437cc1e682a63710c873a073a734a2e7c031
Matt Howe <mhowe@bates.edu>
2020-12-26 12:17:18 -0500

    BTerm Hello world

---
+++ chapters/5/dungeoncrawl/src/main.rs
@@ -0,0 +1,22 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use bracket_lib::prelude::*;
+
+struct State {
+
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, world!");
+    }
+
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Hello, world!")
+        .build()?;
+    main_loop(context, State{})
+}
