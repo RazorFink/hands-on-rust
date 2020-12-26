24d48e28e5f06930cdeb26fd166ce5d6b62c1379
Matt Howe <mhowe@bates.edu>
2020-12-26 16:37:42 -0500

    Add prelude to `main.js`
    Re-export `bracket_lib` and `map`
    Use the current prelude to make it available to the main scope

--- chapters/5/dungeoncrawl/src/main.rs
+++ chapters/5/dungeoncrawl/src/main.rs
@@ -1,6 +1,15 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-use bracket_lib::prelude::*;
+mod map;
+
+pub mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub use crate::map::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+}
+
+use prelude::*;
 
 struct State {
 
