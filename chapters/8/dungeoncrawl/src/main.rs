73d50a64f0ee0106f5cc73a1bffa64ea0ec9433c
Matt Howe <mhowe@bates.edu>
2020-12-30 23:58:34 -0500

    Copy chapter 7 to 8

---
+++ chapters/8/dungeoncrawl/src/main.rs
@@ -0,0 +1,103 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+mod camera;
+mod components;
+mod map;
+mod map_builder;
+mod spawner;
+mod systems;
+mod turn_state;
+
+pub mod prelude {
+    pub use crate::camera::*;
+    pub use crate::components::*;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
+    pub use crate::turn_state::*;
+    pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+}
+
+use prelude::*;
+
+struct State {
+    ecs: World,
+    resources: Resources,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule,
+}
+
+impl State {
+    fn new() -> Self {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::build(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
+        resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(map_builder.map);
+        resources.insert(TurnState::AwaitingInput);
+        Self {
+            ecs,
+            resources,
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler(),
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
+        ctx.cls();
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.resources.insert(ctx.key); // insert replaces any resource of the same type
+                                        // Get an Option for resource of type TurnState
+                                        // upwrap() / destructure the Option to get a &TurnState
+                                        // Clone the borrowed &TurnState
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+        match current_state {
+            TurnState::AwaitingInput => self
+                .input_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::PlayerTurn => self
+                .player_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::MonsterTurn => self
+                .monster_systems
+                .execute(&mut self.ecs, &mut self.resources),
+        }
+        render_draw_buffer(ctx).expect("Render Error");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::new()
+        .with_title("Dungeon Crawler")
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .build()?;
+
+    main_loop(context, State::new())
+}
---
+++ chapters/8/dungeoncrawl/src/systems/player_input.rs
@@ -0,0 +1,39 @@
+use crate::prelude::*;
+//Procedural macro from the Legion lib
+// appends _system to the fn name
+#[system]
+#[read_component(Point)]
+#[read_component(Player)]
+pub fn player_input(
+    ecs: &mut SubWorld,
+    commands: &mut CommandBuffer,
+    #[resource] key: &Option<VirtualKeyCode>,
+    #[resource] turn_state: &mut TurnState,
+) {
+    if turn_state.clone() != TurnState::AwaitingInput {
+        return;
+    }
+    if let Some(key) = key {
+        let delta = match key {
+            VirtualKeyCode::Left => Point::new(-1, 0),
+            VirtualKeyCode::Right => Point::new(1, 0),
+            VirtualKeyCode::Up => Point::new(0, -1),
+            VirtualKeyCode::Down => Point::new(0, 1),
+            _ => Point::zero(),
+        };
+        if delta.x != 0 || delta.y != 0 {
+            let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+            players.iter(ecs).for_each(|(entity, pos)| {
+                let destination = *pos + delta;
+                commands.push((
+                    (),
+                    WantsToMove {
+                        entity: *entity,
+                        destination,
+                    },
+                ));
+            });
+        }
+        *turn_state = TurnState::PlayerTurn;
+    }
+}
